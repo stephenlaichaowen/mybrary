b436a0ce128369ddf0cf1795d1a60bc7f044cdfd
stephenlai <stephenlai2015@gmail.com>
2020-03-19 14:18:10 +0800

    add content to mogoose.connect()

--- app.js
+++ app.js
@@ -13,8 +13,10 @@ const engine = require('ejs-locals')
 const indexRouter = require('./routes/index')
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.DATABASE_URL, { 
-  useNewUrlParser: true
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
 })
 const db = mongoose.connection
 db.on('error', error => console.error(error))
